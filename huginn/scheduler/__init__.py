795fc4beaaabcc9e42f211a25db7fdf44f7561a5
glen <glen@hpznt.com>
2026-03-17 00:29:36 +0800

    F-001: Add APScheduler dependency and create scheduler package skeleton

---
+++ huginn/scheduler/__init__.py
@@ -0,0 +1,6 @@
+"""Huginn scheduler module.
+
+Provides task scheduling capabilities for automated spider runs using APScheduler.
+"""
+
+__all__ = []
---
+++ tests/test_scheduler/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for huginn.scheduler module."""
