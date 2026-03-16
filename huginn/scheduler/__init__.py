0900b70422476ef90d565fc5b9df0a716ab65d44
glen <glen@hpznt.com>
2026-03-17 04:20:05 +0800

    Export SchedulerManager from scheduler module

--- huginn/scheduler/__init__.py
+++ huginn/scheduler/__init__.py
@@ -5,5 +5,6 @@ Provides task scheduling capabilities for automated spider runs using APSchedule
 
 from huginn.scheduler.alert import send_alert
 from huginn.scheduler.cron import parse_cron
+from huginn.scheduler.manager import SchedulerManager
 
-__all__ = ["parse_cron", "send_alert"]
+__all__ = ["parse_cron", "send_alert", "SchedulerManager"]
